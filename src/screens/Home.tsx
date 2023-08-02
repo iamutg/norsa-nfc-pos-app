5f35681e8aa69f4e59fd16e9b87e97bad01c235c
musman97 <zahoor.usman.m@gmail.com>
2023-08-02 21:31:09 +0500

    Change no issuance found message

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -158,7 +158,7 @@ const Home: FC<Props> = ({navigation: {navigate}}) => {
       showToast(issuanceHistoriesRes?.message);
     } else if (issuanceHistoriesRes?.data?.length === 0) {
       showToast(
-        'This person has not applied for a transaction, Please contact dealer.',
+        'This person has not applied to perform a transaction, Please contact your dealer',
       );
     } else {
       const paybackPickerItems: Array<PickerItem> =
