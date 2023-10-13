8f73349ed677f2c7e0d5253b5ebb03f9a440ed8d
musman97 <zahoor.usman.m@gmail.com>
2023-10-13 11:11:06 +0500

    refactor: Use `__DEV__` for Base url selection

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,9 @@
 import {AppMode} from '~/types';
 
-const isDev = true;
-
 const DEV_BASE_URL = 'https://norsa-backend-demo.herokuapp.com/api/';
 const PROD_BASE_URL = 'https://norsabackend.herokuapp.com/api/';
 
-export const BASE_URL = isDev ? DEV_BASE_URL : PROD_BASE_URL;
+export const BASE_URL = __DEV__ ? DEV_BASE_URL : PROD_BASE_URL;
 
 export const authEndpoints = {
   login: 'auth/login',
