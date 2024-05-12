071be02e3f3f7728f98de7bb9f6cf0537c6b379a
musman97 <zahoor.usman.m@gmail.com>
2024-05-12 17:19:53 +0500

    chore: Uncomment hardcoded base url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,8 +3,8 @@ import {AppMode} from '~/types';
 const DEV_BASE_URL = 'https://norsa-backend-demo.herokuapp.com/api/';
 const PROD_BASE_URL = 'https://norsabackend.herokuapp.com/api/';
 
-// export const BASE_URL = __DEV__ ? DEV_BASE_URL : PROD_BASE_URL;
-export const BASE_URL = PROD_BASE_URL;
+export const BASE_URL = __DEV__ ? DEV_BASE_URL : PROD_BASE_URL;
+// export const BASE_URL = PROD_BASE_URL;
 
 export const authEndpoints = {
   login: 'auth/login',
