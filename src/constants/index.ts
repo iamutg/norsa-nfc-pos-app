ddb1fa0c53b54a5c4409a0838b6cfe77cc40d09b
Muhammad Usman <zahoor.usman.m@gmail.com>
2024-08-24 12:31:05 +0500

    feat: Update backend base url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import {AppMode} from '~/types';
 
-const DEV_BASE_URL = 'https://norsa-backend-demo.herokuapp.com/api/';
-const PROD_BASE_URL = 'https://norsabackend.herokuapp.com/api/';
+const DEV_BASE_URL = 'https://api.merpol.org/';
+const PROD_BASE_URL = 'https://api.merpol.org/';
 
 export const BASE_URL = __DEV__ ? DEV_BASE_URL : PROD_BASE_URL;
 
