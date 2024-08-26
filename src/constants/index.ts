9759d7e60dc404758308c55465b265ca279883cf
Muhammad Usman <zahoor.usman.m@gmail.com>
2024-08-26 21:03:28 +0500

    fix: Fix api base url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import {AppMode} from '~/types';
 
-const DEV_BASE_URL = 'https://norsa-backend-demo.herokuapp.com/api/';
-const PROD_BASE_URL = 'https://norsabackend.herokuapp.com/api/';
+const DEV_BASE_URL = 'https://api.merpol.org/api/';
+const PROD_BASE_URL = 'https://api.merpol.org/api/';
 
 export const BASE_URL = __DEV__ ? DEV_BASE_URL : PROD_BASE_URL;
 
