4188dcb82ecbdd5184af0137940e84a1aa4c1189
iamutg <91552108+iamutg@users.noreply.github.com>
2025-10-15 10:10:50 -0400

    Update MainActivity.java
    removed navigation buttons

--- android/app/src/main/java/com/posprinter/MainActivity.java
+++ android/app/src/main/java/com/posprinter/MainActivity.java
@@ -9,7 +9,25 @@ public class MainActivity extends ReactActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
   }
+import android.os.Bundle;
+import android.view.View;
 
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    hideNavigationBar();
+}
+
+private void hideNavigationBar() {
+    View decorView = getWindow().getDecorView();
+    decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+}
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
